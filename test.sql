da497e8763558a739fe4494340598769865a33d8
Yorbit <marco.95112@hotmail.it>
2022-09-06 14:52:34 +0200

    end 4

--- test.sql
+++ test.sql
@@ -5,15 +5,22 @@
     FROM `students`
     WHERE YEAR(`students`.`date_of_birth`) = 1990;
 
-
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-
+    SELECT *
+    FROM `courses`
+    WHERE `courses`.`cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+    SELECT *
+    FROM `students`
+    WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30
+    ORDER BY `date_of_birth` DESC;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+    SELECT * 
+    FROM `courses`
+    WHERE `year` = 1
+    AND `period` = "I semestre";
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
